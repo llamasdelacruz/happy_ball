68372ca75fd6297c043d78e7798cd8f1731f35f7
llamasdelacruz <tetillamas@gmail.com>
2024-03-14 21:48:16 -0600

    ventanas combinadas

--- app/src/main/java/com/example/happy_ball/MainActivity.java
+++ app/src/main/java/com/example/happy_ball/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.happy_ball;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final Button btnSkins = (Button) findViewById(R.id.BtnSkins);
+
+        btnSkins.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, Skins.class);
+                startActivity(intent);
+            }
+        });
     }
 }
